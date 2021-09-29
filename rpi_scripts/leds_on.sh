4dde71bde20583dc26eb9b29c5c5e1f427942b5d
Andrew Miotke <andrewmiotke@gmail.com>
2021-09-28 17:25:12 -0700

    leds_on: Replaced the word 'off' for 'on'. This mistake happened because I copied the leds_off.sh script to create this one.

--- rpi_scripts/leds_on.sh
+++ rpi_scripts/leds_on.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This script turns off the green (led 0) and the red (led 1) leds on the front the Raspberry Pi 4
+# This script turns on the green (led 0) and the red (led 1) leds on the front the Raspberry Pi 4
 
 sudo sh -c 'echo 1 > /sys/class/led0/brightness'
-echo "Turning off green led."
+echo "Turning on green led."
 
 sudo sh -c 'echo 1 > /sys/class/led1/brightness'
-echo "Turning off red led."
+echo "Turning on red led."

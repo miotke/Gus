4765c0773f4fd8130d87102d9eb5e681b5783b0c
Andrew Miotke <andrewmiotke@gmail.com>
2021-09-30 08:29:54 -0700

    RPi4 Scripts: Fixed directory typo in both off and on scripts

--- rpi_scripts/leds_off.sh
+++ rpi_scripts/leds_off.sh
@@ -2,8 +2,8 @@
 
 # This script turns off the green (led 0) and the red (led 1) leds on the front the Raspberry Pi 4
 
-sudo sh -c 'echo 0 > /sys/class/led0/brightness'
+sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'
 echo "Turning off green led."
 
-sudo sh -c 'echo 0 > /sys/class/led1/brightness'
+sudo sh -c 'echo 0 > /sys/class/leds/led1/brightness'
 echo "Turning off red led."
